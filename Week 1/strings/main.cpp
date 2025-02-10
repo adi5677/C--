862dbec2463e88f59be4d8e5356bde4cb40f2c38
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-02-10 14:17:48 +0530

    string in c++

---
+++ Week 1/strings/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+    //Decleare
+    // string name;
+
+    // name.push_back('a');
+    // name.push_back('b');
+    // name.push_back('c');
+    // name.push_back('d');
+    // cout << name << endl;
+
+
+    //library function of string 
+    string str = "Aditya";
+    string temp = "Raj";
+    str.append(temp);
+    cout << str << endl;
+
+    // cout << "length: " << str.length() << endl;
+
+
+    
+    //initalisation 
+    // string surname = "Sharma"
+
+    //updation
+    // surname = "Raj"
+
+
+
+
+    return 0;
+}
